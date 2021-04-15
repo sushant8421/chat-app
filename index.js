286ac56e82f5d84445aa94192c3996f2e253f033
bbsushant8421 <sushant1.yadav@paytm.com>
2021-04-15 08:07:53 +0530

    prot set in environment variable

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+const PORT = process.env.PORT || 3000;
+var server = app.listen(PORT, function(){
+    console.log(`listening for requests on ${PORT}`);
 });
 
 // Static files
