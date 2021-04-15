327d939fd6eda2fedc4638b3ddcf519d94762c53
bbsushant8421 <sushant1.yadav@paytm.com>
2021-04-15 09:03:57 +0530

    cors added

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var socket = require('socket.io');
+var path = require( 'path' );
 
 // App setup
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain); 
+
+
 const PORT = process.env.PORT || 3000;
 var server = app.listen(PORT, function(){
     console.log(`listening for requests on ${PORT}`);
 });
 
 // Static files
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Socket setup & pass server
 var io = socket(server);
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect(window.location.hostname);
 
 // Query DOM
 var message = document.getElementById('message'),
