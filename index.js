aeea312ffaad5dd6e417a4f7e23a725e7f075f5b
bbsushant8421 <sushant1.yadav@paytm.com>
2021-04-17 11:41:50 +0530

    cors added

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var path = require( 'path' );
 
 // App setup
 var app = express();
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', "*");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// }
 
-// app.use(allowCrossDomain); 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain); 
 
 const PORT = process.env.PORT || 3000;
 var server = app.listen(PORT, function(){
